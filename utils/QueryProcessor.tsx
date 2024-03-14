db400a6eead61c3df36e262542d94f69097a2fde
Tarun Bandi <tabandi2004@gmail.com>
2024-03-14 16:26:23 -0400

    Solve subtract and power

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,3 +1,11 @@
+function isSquareAndCube(num : number)
+{
+  let sqrt = Math.pow(num, 1/2);
+  let crt = Math.pow(num, 1/3);
+  console.log(num,sqrt,crt);
+  return Number.isInteger(sqrt) && Number.isInteger(crt);
+}
+
 export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("shakespeare")) {
       return (
@@ -50,6 +58,48 @@ export default function QueryProcessor(query: string): string {
     } else {
         return 'Invalid query format';
     }
+  }else if ((query.toLowerCase().includes("minus")))
+  {
+    const regex = /What is (\d+) minus (\d+)\?/;
+    const match = query.match(regex);
+
+    if (match) {
+        const num1 = parseInt(match[1]);
+        const num2 = parseInt(match[2]);
+
+        const result = num1 - num2;
+        return result.toString();
+    } else {
+        return 'Invalid query format';
+    }
+  }else if ((query.toLowerCase().includes("power")))
+  {
+    const regex = /What is (\d+) to the power of (\d+)\?/;
+    const match = query.match(regex);
+
+    if (match) {
+        const num1 = parseInt(match[1]);
+        const num2 = parseInt(match[2]);
+
+        const result = num1 ** num2;
+        return result.toString();
+    } else {
+        return 'Invalid query format';
+    }
+  } 
+  else if ((query.toLowerCase().includes("square")))
+  {
+    const regex = /Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)\?/
+    const match = query.match(regex);
+    if (match) {
+      for (let i = 1; i < 8; i++)
+      {
+        if (isSquareAndCube(parseInt(match[i])))
+          return match[i].toString();
+      }
+      return "None were squares or cubes";
+    }
+      return "Invalid query";
   }
 
   return "";
